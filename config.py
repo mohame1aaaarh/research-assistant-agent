ba974bd4a33087b87a74c7962800da4d152683b2
abdallah <2006anamf3@gmail.com>
2026-02-25 21:08:02 +0200

    Add The API from my desktop

--- config.py
+++ config.py
@@ -1,4 +1,15 @@
-GEMINI_API_KEY = "ضع الـ API Key هنا"
+import os
+
+path_to_key = os.path.expanduser("~/Desktop/my_api_key.txt")
+
+try:
+    with open(path_to_key, "r") as file:
+        API_KEY = file.read().strip()
+except FileNotFoundError:
+    print("خطأ: لم يتم العثور على ملف المفتاح في سطح المكتب!")
+    API_KEY = None
+
+GEMINI_API_KEY = API_KEY
 APP_NAME = "Research Assistant Agent"
 DATABASE_PATH = "database/conversations.db"
 MAX_SEARCH_RESULTS = 5

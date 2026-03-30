b34a2c820df6408a8215df249511620a1942047c
abdallah <2006anamf3@gmail.com>
2026-03-30 15:41:50 +0200

    Adding API Key

--- .history/config_20260330154109.py
+++ .history/config_20260330154109.py
@@ -1,5 +1,5 @@
 import os
-OPENROUTER_KEY: str = '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'
+OPENROUTER_KEY: str = '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'
 APP_NAME: str = "Research Assistant Agent"
 DATABASE_PATH: str = "database/conversations.db"
 MAX_SEARCH_RESULTS: int = 5
